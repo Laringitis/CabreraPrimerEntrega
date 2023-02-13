649812a2643fbf5e9f6a2c2b4d2838eb00e4eae7
Laringitis <115123448+Laringitis@users.noreply.github.com>
2023-02-13 16:33:39 -0300

    bucle

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let usuarioIngresado= prompt("Ingrese su nombre de entrenador pokemon ");
 
-let numeroPokedex=prompt("Ingrese el numero de pokedex de su pokemon (del 1 al 386)");
+let numeroPokedex=prompt("Ingrese un número para verificar la region de su pokemon (del 1 al 386)");
     if(numeroPokedex>=387){
     alert("Todavia no desbloqueo los pokemones de esta región");
     }else if(numeroPokedex>=1 && numeroPokedex<=151){
@@ -17,27 +17,25 @@ let numeroPokedex=prompt("Ingrese el numero de pokedex de su pokemon (del 1 al 3
     }
 
 let tipoPokemon= prompt ("¿Su pokemon es fuego, agua, tierra o eléctrico?")
-
-        switch (tipoPokemon){
-            case "fuego":
-                let fuegoTipo= " y es tipo fuego (es debil a pokemones tipo agua, tierra y roca)"
-                clasificacionPokemon=fuegoTipo
-                break;
-            case "agua":
-                let aguaTipo= " y es tipo agua (es debil a pokemones tipo eléctrico y planta)"
-                clasificacionPokemon=aguaTipo
-                break;
-            case "tierra":
-                let tierraTipo= " y es tipo tierra (es debil a  pokemones tipo agua, hielo y planta)"
-                clasificacionPokemon=tierraTipo
-                break;
-            case "eléctrico":
-            case "electrico":
-                let electricoTipo= " y es tipo eléctrico (es debil a pokemones tipo tierra)"
-                clasificacionPokemon=electricoTipo
-                break;
-            default:
-                alert("No inserto ninguna clasificación de pokemon")
-        }
-
+    switch (tipoPokemon){
+        case "fuego":
+            let fuegoTipo= " y es tipo fuego (es debil a pokemones tipo agua, tierra y roca)"
+            clasificacionPokemon=fuegoTipo
+            break;
+        case "agua":
+            let aguaTipo= " y es tipo agua (es debil a pokemones tipo eléctrico y planta)"
+            clasificacionPokemon=aguaTipo
+            break;
+        case "tierra":
+            let tierraTipo= " y es tipo tierra (es debil a  pokemones tipo agua, hielo y planta)"
+            clasificacionPokemon=tierraTipo
+            break;
+        case "eléctrico":
+        case "electrico":
+            let electricoTipo= " y es tipo eléctrico (es debil a pokemones tipo tierra)"
+            clasificacionPokemon=electricoTipo
+            break;
+}
+for (var i = 0; i < 5; i++){
 document.write("Bienvenido " + usuarioIngresado + " "+ regionPokedex + clasificacionPokemon)
+}
